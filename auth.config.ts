d076f4ce15d3fca919fc798b4de28bb509bee2ba
TKD04 <tkd04fordev@outlook.com>
2024-06-10 18:27:37 +0900

    Fix typo ('./dashbard' -> '/dashboard')

--- auth.config.ts
+++ auth.config.ts
@@ -7,7 +7,7 @@ export const authConfig: NextAuthConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('./dashboard');
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
 
       if (isOnDashboard) {
         if (isLoggedIn) {
