222e9e3549bf124db3a6c5ff521987cffc446db5
TKD04 <tkd04fordev@outlook.com>
2024-06-10 19:41:53 +0900

    Fix typo ('Invoice' -> 'Invoices')

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Invoice',
+  title: 'Invoices',
 };
 
 export default async function Page({
